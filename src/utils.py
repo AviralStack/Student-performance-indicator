ffc405ea6b35bad1e63f43ab3ffb47d391f924da
Aviral Gupta <gaviral172@gmail.com>
2026-04-05 14:28:28 +0000

    Deployment Configration

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 import dill
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
+import pickle
 
 from src.exception import CustomException
 def save_object(file_path,obj): 
@@ -33,4 +34,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
      except Exception as e:
           raise CustomException(e,sys)
 
-    
+
+   
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
